14bae555bdcc9fbd0a15db9f848db685dcc8a1c2
Mike Hartington <mikehartington@gmail.com>
2020-08-19 12:44:20 -0400

    add example

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public filterVal: number = 0;
 
-  constructor() {}
-
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    public location: Location,
+  ) {}
+  ngOnInit() {
+    const filter = this.activatedRoute.snapshot.queryParamMap.get('filter');
+    if (filter) {
+      this.filterVal = parseInt(filter);
+    }
+  }
+  increment() {
+    this.filterVal += 1;
+    this.navigate();
+  }
+  decrement() {
+    this.filterVal -= 1;
+    this.navigate();
+  }
+  navigate() {
+    const navExtras: NavigationExtras = {
+      queryParams: { filter: this.filterVal },
+    };
+    this.router.navigate([], { ...navExtras });
+  }
 }
